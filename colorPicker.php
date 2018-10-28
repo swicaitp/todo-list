4b95178ad2fd74529e109d035b7c7f98619d8925
kokokonotsu <ethansown@gmail.com>
2018-10-28 13:08:30 -0500

    Experimenting with PHP File Handling

--- colorPicker.php
+++ colorPicker.php
@@ -1,3 +1,6 @@
 <?php
-$color;
+$cssFile = fopen("styles.CSS", "a") or die("Unable to open CSS file!");
+$htmlFile = fopen("index.HTML", "r") or die ("Unable to open HTML file!")
+$htmlTxt =
+$cssTxt = ".colorPicked{ background-color:" +  }
 ?>
