0d6b4d65755cb065c0ff5d6f9a503b32e16443dd
Ethan Simpson <ethansown@gmail.com>
2018-11-12 23:00:15 -0600

    Added Glow effect to Top Title

--- scripts.js
+++ scripts.js
@@ -238,7 +238,96 @@ if(inputLength() > 0 && event.which === 13){
   createListElement();
 }
 }
+function clock(){
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  console.log(canvas.height);
+  var radius = canvas.height / 2;
+  console.log(radius);
+  ctx.translate(radius, radius);
+  radius *= 0.90;
+  console.log(radius);
+  drawClock();
 
+  function drawClock() {
+      drawFace(ctx, radius);
+      drawNumbers(ctx, radius);
+      drawTime(ctx, radius);
+  }
+  function drawFace(ctx, radius){
+      var grad;
+      //Start Backface
+      ctx.beginPath();
+      ctx.arc(0, 0, radius, 0, 2*Math.PI);
+      ctx.fillStyle = headerColorValue;
+      ctx.fill();
+      //End Backface
+      //Start Frontface
+      //Start Outer Rim
+      grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+      grad.addColorStop(0, highLightColorValue);
+      grad.addColorStop(0.5, borderColorValue);
+      grad.addColorStop(1.0, highLightColorValue);
+      ctx.strokeStyle = grad;
+      ctx.lineWidth = radius * 0.1;
+      ctx.stroke();
+      //End Outer Rim
+      //Start Hand Base
+      ctx.beginPath();
+      ctx.arc(0, 0, radius*0.1, 0, 2*Math.PI);
+      ctx.fillStyle = borderColorValue;
+      ctx.fill();
+      //End Hand Base
+      //End Frontface
+  }
+  function drawNumbers(ctx, radius){
+      var ang;
+      var num;
+      ctx.font = radius*0.15 + "px arial";
+      ctx.textBaseline = "middle";
+      ctx.textAlign = "center";
+      for(num = 1; num < 13; num++){
+          ang = num * Math.PI / 6;
+          ctx.rotate(ang);
+          ctx.translate(0, -radius * 0.85);
+          ctx.rotate(-ang);
+          ctx.fillText(num.toString(), 0, 0);
+          ctx.rotate(ang);
+          ctx.translate(0, radius * 0.85);
+          ctx.rotate(-ang);
+      }
+  }
+  function drawTime(ctx, radius){
+      var now = new Date();
+      var hour = now.getHours();
+      var minute = now.getMinutes();
+      var second = now.getSeconds();
+      //Hour Start
+      hour = hour % 12;
+      hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (6 * 360));
+      drawHand(ctx, hour, radius * 0.5, radius * 0.07);
+      //Hour End
+      //Minute Start
+      minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+      drawHand(ctx, minute, radius * 0.8, radius * 0.07);
+      //Minute End
+      //Second Start
+      second = (second * Math.PI / 30);
+      drawHand(ctx, second, radius * 0.9, radius * 0.02);
+  }
+  function drawHand(ctx, pos, length, width){
+      ctx.beginPath();
+      ctx.lineWidth = width;
+      ctx.lineCap = "round";
+      ctx.moveTo(0, 0);
+      ctx.rotate(pos);
+      ctx.lineTo(0, -length);
+      ctx.stroke();
+      ctx.rotate(-pos);
+  }
+  setInterval(drawClock, 1000);
+}
+window.addEventListener("load", clock);
 
 enterButton.addEventListener("click", addListAfterClick);
 taskInput.addEventListener("keypress", addListAfterEvent);
