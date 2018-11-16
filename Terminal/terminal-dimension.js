b5d4009842787de350a457514be8253155eeec38
Ethan Simpson <ethansown@gmail.com>
2018-11-16 14:35:04 -0600

    Implementing Async Functions and Promises

--- Terminal/terminal-dimension.js
+++ Terminal/terminal-dimension.js
@@ -1,5 +1,16 @@
-// const terminalInput = document.getElementById("terminal-input");
-// const helperTerminal = document.getElementById("helper-terminal");
+const terminalInput = document.getElementById("terminal-input");
+const helperTerminal = document.getElementById("helper-terminal");
+const helperMessageOutput = document.getElementsByClassName("helper-message")[0];
+const helperMessages = {text: "OH, you're here..."};
+function welcomeMessage(){
+  return new Promise(resolve => { setTimeout(() => { resolve(helperMessages.text);}, 2000);});
+}
+async function asyncCall(){
+  console.log("calling...");
+  var result = await welcomeMessage();
+  helperMessageOutput.innerHTML = result;
+}
+asyncCall();
 //   terminalInput.innerHTML = "";
 // function terminalInputStart(){
 //   terminalInput.innerHTML = "C:\\Users\\Meat-Bag>";
