b0b5c59916b0c22306d7cb6d18df67d65ba1c1ad
Ethan Simpson <ethansown@gmail.com>
2018-11-16 14:12:00 -0600

    Trying a Different Approach

--- Terminal/terminal-dimension.js
+++ Terminal/terminal-dimension.js
@@ -1,4 +1,19 @@
-const terminalInput = document.getElementById("terminal-input");
-function terminalInputStart(){
-  terminalInput.innerHTML
-}
+// const terminalInput = document.getElementById("terminal-input");
+// const helperTerminal = document.getElementById("helper-terminal");
+//   terminalInput.innerHTML = "";
+// function terminalInputStart(){
+//   terminalInput.innerHTML = "C:\\Users\\Meat-Bag>";
+// }
+// var terminalInputNextLine = function(event){
+//   const keyName = "Enter";
+//   const breakLine = document.createElement("br");
+//   const nextLine = "C:\\Users\\Meat-Bag>";
+//   if(event.key == keyName){
+//     terminalInput.insertAdjacentElement("afterend", breakLine);
+//     terminalInput.appendChild(nextLine);
+//     console.log("I am running");
+//   }
+// }
+// window.addEventListener("load", terminalInputStart);
+// terminalInput.addEventListener("keydown", terminalInputNextLine);
+
