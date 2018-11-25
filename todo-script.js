556d724b9f67b9f1a9e8de8f097b4553272aa1b1
Ethan Simpson <ethansown@gmail.com>
2018-11-25 14:08:21 -0600

    Fixed Multiple Todo's not highlighting bug

--- todo-script.js
+++ todo-script.js
@@ -1,6 +1,16 @@
 const lastLi = document.querySelectorAll("li");
-const thisLi = lastLi[lastLi.length - 1];
 function crossOut(){
   this.classList.toggle("done");
+  if(this.classList.contains("done")){
+    this.style.borderColor = "#FFB000";
+    for(let i = 0; i > this.children.length; i++){
+      this.children[i].style.fontColor = "#FFB000";
+      this.children[i].style.borderColor = "#FFB000";
+    }
+  } else {
+    this.style.borderColor = "#32CD32";
+  }
 }
-thisLi.addEventListener("click", crossOut);
+for(let i = 0; i < lastLi.length; i++){
+  lastLi[i].addEventListener("click", crossOut);
+}
