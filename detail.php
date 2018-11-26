51e0b39d34c876f1dfbd96d17cbae44986b7abbe
Ethan Simpson <ethansown@gmail.com>
2018-11-25 20:01:06 -0600

    Added Functionality with Competed Tasks

--- detail.php
+++ detail.php
@@ -14,24 +14,35 @@
       $id = $_GET["id"];
       db();
       global $link;
-      $query = "SELECT todoTitle, todoDescription, date FROM todo WHERE id='$id'";
+      $query = "SELECT todoTitle, todoDescription, time, date FROM todo WHERE id='$id'";
       $result = mysqli_query($link, $query);
       if(mysqli_num_rows($result)==1){
         $row = mysqli_fetch_array($result);
         if($row){
           $title = $row["todoTitle"];
           $description = $row["todoDescription"];
+          $time = $row["time"];
           $date = $row["date"];
 
-          echo "<h2>$title</h2><p>$description</p><p>$date</p>";
+          echo "<h2>$title</h2><p>$description</p><p>$time</p><p>$date</p>";
         }
-      } else {
-        echo "No Task";
       }
-    } else {
-      echo "You must use an id";
+      $query_complete = "SELECT todoTitle, todoDescription, time, date FROM complete WHERE id = '$id'";
+      $result_complete = mysqli_query($link, $query_complete);
+      if(mysqli_num_rows($result_complete)==1){
+        $row = mysqli_fetch_array($result_complete);
+        if($row){
+          $title = $row["todoTitle"];
+          $description = $row["todoDescription"];
+          $time = $row["time"];
+          $date = $row["date"];
+          echo "<h2>$title</h2><p>$description</p><p>$time</p><p>$date</p>";
+        }
     }
-  ?>
+  } else {
+    echo "You must use an id";
+  }
+?>
   <a href="index.php">Back to Task List</a>
 </main>
 <footer>
